0aa72ac3dd8de0e3b10987537ac37f66097cc186
Lorenzosaenz09 <lorenzosaenz2009@gmail.com>
2026-05-29 10:17:49 -0300

    Clase 2: Mecánica de recolección funcional y modelo con materiales

---
+++ Assets/Scripts/Recolector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recolector : MonoBehaviour
+{
+    int contador = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    
+    {
+        if (other.CompareTag("Coleccionable"))
+        {
+            Destroy(other.gameObject);
+
+            contador++;
+
+            Debug.Log("Objetos recolectados: " + contador);
+        }
+    }
+}
